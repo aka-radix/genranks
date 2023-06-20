8d87f5c38d40517d274e2ac5d20e5b7ef612449c
Mart <m@rtin.win>
2023-06-20 09:20:24 +0200

    Made leaderboards prettier

--- resources/views/livewire/leaderboard.blade.php
+++ resources/views/livewire/leaderboard.blade.php
@@ -1,46 +1,61 @@
-<div class="px-4 py-8 bg-white dark:bg-gray-800 sm:px-6 lg:px-8">
-    <h2 class="mb-4 text-2xl font-semibold">Leaderboard</h2>
-
-    <div class="overflow-x-auto">
-        <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-            <!-- Table headers -->
-            <thead class="bg-gray-100 dark:bg-gray-700">
+<div>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div class="pb-4 bg-white dark:bg-gray-900">
+            <label for="table-search" class="sr-only">Search</label>
+            <div class="relative mt-1">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <x-icon icon='search' />
+                </div>
+                <input wire:model.debounce="search" type="text" id="table-search"
+                    class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Search for items">
+            </div>
+        </div>
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                    <th
-                        class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
-                        ELO</th>
-                    <th
-                        class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
-                        Rank</th>
-                    <th
-                        class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
-                        Nickname</th>
-                    <th
-                        class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
-                        Games Played</th>
+                    <th scope="col" class="px-6 py-3">
+                        Rank
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Nickname
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        ELO
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Games Played
+                    </th>
                 </tr>
             </thead>
-
-            <!-- Table body -->
-            <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                @forelse ($leaderboard as $user)
-                    <tr>
-                        <td class="px-6 py-4 whitespace-nowrap">{{ $user->elo }}</td>
-                        <td class="px-6 py-4 whitespace-nowrap">{{ $user->rank }}</td>
-                        <td class="px-6 py-4 whitespace-nowrap">{{ $user->nickname }}</td>
-                        <td class="px-6 py-4 whitespace-nowrap">{{ $user->games_played }}</td>
+            <tbody>
+                @forelse ($users as $user)
+                    <tr
+                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="px-6 py-4">
+                            {{ $user->rank }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $user->nickname }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $user->elo }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $user->games_played }}
+                        </td>
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="4" class="px-6 py-4 text-center">No users on the leaderboard.</td>
+                        <td colspan="5" class="px-6 py-4 text-center">No users found.</td>
                     </tr>
                 @endforelse
             </tbody>
         </table>
     </div>
-
-    <!-- Display pagination links -->
-    <div class="mt-4">
-        {{ $leaderboard->links() }}
-    </div>
+    @if ($users->hasPages())
+        <nav class="flex items-center justify-between pt-4" aria-label="Table navigation">
+            {{ $users->links() }}
+        </nav>
+    @endif
 </div>
