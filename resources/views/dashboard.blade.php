71b79a83a48be0c65e5d9abdefecccbac41d6568
Mart <m@rtin.win>
2023-06-20 14:59:32 +0200

    Cleaner dashboard and leaderboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,7 @@
 
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-xl dark:bg-gray-800 sm:rounded-lg">
-                <x-welcome />
-                <x-gen-tool-timer class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200" />
+            <div class="overflow-hidden sm:rounded-lg">
                 <livewire:leaderboard />
             </div>
         </div>
--- resources/views/livewire/leaderboard.blade.php
+++ resources/views/livewire/leaderboard.blade.php
@@ -1,14 +1,18 @@
 <div>
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <div class="pb-4 bg-white dark:bg-gray-900">
+        <div class="items-center py-4 mx-1 md:justify-between md:flex">
             <label for="table-search" class="sr-only">Search</label>
-            <div class="relative mt-1">
+            <div class="relative">
                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <x-icon icon='search' />
                 </div>
                 <input wire:model.debounce="search" type="text" id="table-search"
-                    class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Search for items">
+                    class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg md:w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Search for users" name="users">
+            </div>
+            <div>
+                <x-gen-tool-timer
+                    class="mt-4 text-xl font-semibold leading-tight text-gray-800 md:mt-0 dark:text-gray-200" />
             </div>
         </div>
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
