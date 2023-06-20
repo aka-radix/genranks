2dd19fd8711aa0b0a9829990b991e1b2e11c01dc
Mart <m@rtin.win>
2023-06-20 09:19:54 +0200

    Added icons

---
+++ resources/views/components/icon.blade.php
@@ -0,0 +1,6 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="{{ $width }}" height="{{ $height }}"
+    viewBox="0 0 {{ $viewBox }}" fill="{{ $fill }}" stroke="currentColor" stroke-width="{{ $strokeWidth }}"
+    stroke-linecap="round" stroke-linejoin="round" id="{{ $id }}"
+    {{ $attributes->merge(['class' => "icon icon-$icon $class"]) }}>
+    @includeIf("icons.$icon")
+</svg>
