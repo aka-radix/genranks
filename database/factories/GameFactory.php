b0ecb41fa60d1a7956e3a0e42051efbc4ced5820
Mart <m@rtin.win>
2023-06-21 09:46:22 +0200

    Fixed Game factory naming

--- database/factories/GameFactory.php
+++ database/factories/GameFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Games>
  */
-class GamesFactory extends Factory
+class GameFactory extends Factory
 {
     /**
      * Define the model's default state.
