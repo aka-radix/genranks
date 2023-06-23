2b3321e6d11aee27cbca7fc3970274f4a88f631e
Mart <m@rtin.win>
2023-06-24 00:42:34 +0200

    Added verified field to games

--- database/migrations/2023_06_19_185030_create_games_table.php
+++ database/migrations/2023_06_19_185030_create_games_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('games', function (Blueprint $table) {
             $table->id();
             $table->foreignId('winner_id')->nullable();
+            $table->boolean('verified')->default(false);
             $table->string('map');
             $table->timestamps();
         });
