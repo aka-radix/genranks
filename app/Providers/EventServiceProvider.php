c9e75f3e8e302714670d670ddc6333a29a9e4121
Mart <m@rtin.win>
2023-06-24 16:37:54 +0200

    Fixed event listeners namespace

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Events\GameCompleted;
 use App\Listeners\AddGameCountToUser;
+use App\Listeners\UpdateUserElo;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
